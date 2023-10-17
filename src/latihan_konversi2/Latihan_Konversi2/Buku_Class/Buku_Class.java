fc1e702832747e725f3b5671a27a77808b7f8227
Blizzard03 <mariq.rasyid@gmail.com>
2023-10-17 11:55:07 +0700

    reconfigure the folder and file project

---
+++ src/latihan_konversi2/Latihan_Konversi2/Buku_Class/Buku_Class.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan_konversi2.Latihan_Konversi2.Buku_Class;
+
+/**
+ *
+ * @author mariq
+ */
+public class Buku_Class {
+     private String jenis;
+    private int keluaran;
+    private int sewaPerHari;
+    private int batas;
+
+    public Buku_Class() {
+        this.jenis = "";
+        this.keluaran = 0;
+        this.sewaPerHari = 0;
+        this.batas = 0;
+    }
+
+    public Buku_Class(String jenis) {
+        this.jenis = jenis;
+        this.keluaran = 0;
+        this.sewaPerHari = 0;
+        this.batas = 0;
+    }
+
+    public Buku_Class(String jenis, int keluaran) {
+        this.jenis = jenis;
+        this.keluaran = keluaran;
+        this.sewaPerHari = 0;
+        this.batas = 0;
+    }
+
+    public Buku_Class(String jenis, int keluaran, int sewaPerHari) {
+        this.jenis = jenis;
+        this.keluaran = keluaran;
+        this.sewaPerHari = sewaPerHari;
+        this.batas = 0;
+    }
+
+    public Buku_Class(String jenis, int keluaran, int sewaPerHari, int batas) {
+        this.jenis = jenis;
+        this.keluaran = keluaran;
+        this.sewaPerHari = sewaPerHari;
+        this.batas = batas;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public void setKeluaran(int keluaran) {
+        this.keluaran = keluaran;
+    }
+
+    public void setSewaPerHari(int sewaPerHari) {
+        this.sewaPerHari = sewaPerHari;
+    }
+
+    public void setBatas(int batas) {
+        this.batas = batas;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public int getKeluaran() {
+        return keluaran;
+    }
+
+    public int getSewaPerHari() {
+        return sewaPerHari;
+    }
+
+    public int getBatas() {
+        return batas;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Keluaran: " + keluaran);
+        System.out.println("Sewa/hari: Rp " + sewaPerHari);
+        System.out.println("Batas: " + batas + " hari");
+    }
+}
---
+++ src/latihan_konversi2/Latihan_Konversi2/Main_Class/Main_Class.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan_konversi2.Latihan_Konversi2.Main_Class;
+
+import latihan_konversi2.Latihan_Konversi2.Buku_Class.Buku_Class;
+
+/**
+ *
+ * @author mariq
+ */
+public class Main_Class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Buku_Class buku1 = new Buku_Class("Pelajaran", 2011, 1000, 7);
+        Buku_Class buku2 = new Buku_Class("Komik", 2007, 2000, 5);
+        Buku_Class buku3 = new Buku_Class("Majalah", 2015, 1500, 2);
+
+        System.out.println("Buku 1:");
+        buku1.tampilkanInfo();
+        System.out.println();
+
+        System.out.println("Buku 2:");
+        buku2.tampilkanInfo();
+        System.out.println();
+
+        System.out.println("Buku 3:");
+        buku3.tampilkanInfo();
+    }
+
+}
